7107e60578da1207a40157864be88d72eca30889
Hitomi Nagano <hitomi.nagano.jp@gmail.com>
2020-07-07 22:10:47 +0900

    add: detail selected page state

--- src/components/VideoDetail/VideoDetail.js
+++ src/components/VideoDetail/VideoDetail.js
@@ -1,18 +1,30 @@
-import React, { useEffect } from "react"
-import { useLocation } from "react-router-dom"
+import React, { useEffect, useContext } from "react";
+import { useLocation } from "react-router-dom";
+import { fetchSelectedData } from "../../apis";
+import { Store } from "../../store";
 
 const VideoDetail = () => {
-  const location = useLocation()
+  const location = useLocation();
+  const { globalState, setGlobalState } = useContext(Store);
+
+  const setSelectedVideo = async () => {
+    const searchParams = new URLSearchParams(location.search);
+    const id = searchParams.get("v");
+    // console.log("id", id)
+    const res = await fetchSelectedData(id);
+    // @see https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Array/shift
+    const item = res.data.items.shift();
+    setGlobalState({
+      type: "SET_SELECTED",
+      payload: { selected: item },
+    });
+  };
 
   useEffect(() => {
-    const searchParams = new URLSearchParams(location.search)
-    const id = searchParams.get("v")
-    console.log("id", id)
-  }, [location.search])
+    setSelectedVideo();
+  }, [setSelectedVideo]);
 
-  return (
-    <div></div>
-  )
-}
+  return <div></div>;
+};
 
-export default VideoDetail
+export default VideoDetail;
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,21 @@
 import React, { createContext, useReducer } from "react"
 
 const initialState = {
-  popular: []
+  popular: [],
+  selected: {}
 }
 
 const reducer = (state, action) => {
   switch (action.type) {
     case "SET_POPULAR":
       return {
+        ...state,
         popular: action.payload.popular
+      }
+    case "SET_SELECTED":
+      return {
+        ...state,
+        selected: action.payload.selected
       }
     default:
       return state
