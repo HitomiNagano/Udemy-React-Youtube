8894feb126e4e954b49fed37e777e61c88cc5ffa
Hitomi Nagano <hitomi.nagano.jp@gmail.com>
2020-07-10 15:06:27 +0900

    コース完了

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,10 +1,47 @@
-import React from "react"
+import React, { useEffect, useCallback, useContext } from "react"
 import Layout from "../components/Layout/Layout"
+import { useLocation } from "react-router-dom"
+import { fetchSearchData } from "../apis"
+import { Store } from "../store"
+import VideoGrid from "../components/VideoGrid/VideoGrid"
+import VideoGridItem from "../components/VideoGridItem/VideoGridItem"
 
 const Search = () => {
+  const location = useLocation()
+  const { globalState, setGlobalState } = useContext(Store)
+
+  const setSearchResult = useCallback(async () => {
+    const searchParams = new URLSearchParams(location.search)
+    const query = searchParams.get("query")
+    if (query) {
+      const res = await fetchSearchData(query)
+      setGlobalState({
+        type: "SET_SEARCHED",
+        payload: {
+          searched: res.data.item
+        }
+      })
+    }
+  }, [location.search, setGlobalState])
+
+  useEffect(() => {
+    setSearchResult()
+  }, [setSearchResult, location.search])
+
   return (
     <Layout>
-      Search page
+      <VideoGrid>
+        {globalState.searched ? globalState.searched.map((search) => {
+          return (
+            <VideoGridItem
+              id={search.id.videoId}
+              key={search.id.videoId}
+              src={search.snippet.thumbnails.medium.url}
+              title={search.snippet.title}
+            />
+          )
+        }): <span>no data</span>}
+      </VideoGrid>
     </Layout>
   )
 }
--- src/store/index.js
+++ src/store/index.js
@@ -3,6 +3,7 @@ import React, { createContext, useReducer } from "react";
 const initialState = {
   popular: [],
   related: [],
+  searched: [],
   selected: {},
   term: "",
 };
@@ -18,6 +19,11 @@ const reducer = (state, action) => {
       return {
         ...state,
         related: action.payload.related,
+      };
+    case "SET_SEARCHED":
+      return {
+        ...state,
+        searched: action.payload.searched,
       };
     case "SET_SELECTED":
       return {
